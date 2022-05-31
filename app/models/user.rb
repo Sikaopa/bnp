59a0b324ea713dadeafdd339809d499b72666cb1
afeez oloyede <oloyedeafeez@363photography.org>
2022-05-31 11:27:48 -0700

    Changed the associations problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :boats, :bookings
+  has_many :boats
+  has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
