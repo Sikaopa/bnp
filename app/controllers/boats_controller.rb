2581068b44c5fdf38747e7d9fd08ab699091bab1
Luca Di Vittorio <luca.div@hotmail.com>
2022-05-31 16:04:01 +0100

    modified the controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,6 +1,8 @@
 class BoatsController < ApplicationController
 
-  def index
+  if params[:query].present?
+    @boats = Boat.where("location ILIKE ?", "%#{params[:query]}%")
+  else
     @boats = Boat.all
   end
 end
