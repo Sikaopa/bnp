c0e1f2f1ea7afdf5b77ba844a823f48b091d704d
George El Nachar <geo.nashar@gmail.com>
2022-05-31 23:18:42 +0100

    unsaved changes to be completedd tomorrow

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :boats, only: [:index,:show]
+  resources :bookings, only:[:index,:new,:create,:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
