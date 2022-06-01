04de7bea4507b37acad780ca418b00d6c723bab3
afeez oloyede <oloyedeafeez@363photography.org>
2022-05-31 17:34:08 -0700

    I created the page to create a new Listing for boat and I also centered the div

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,4 +3,28 @@ class BoatsController < ApplicationController
   def index
     @boats = Boat.all
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user_id = current_user.id
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:make, :location, :image_url, :capacity, :color, :daily_rate)
+  end
 end
