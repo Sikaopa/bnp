2e66e71c18fb8d2e3e9eac275dc6561f5a8513e7
afeez oloyede <oloyedeafeez@363photography.org>
2022-06-05 02:55:06 -0700

    I made the review a reference to the user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
     @boat = Boat.find(params[:boat_id])
     @review = Review.new(review_params)
     @review.boat = @boat
+    @review.user = current_user
     respond_to do |format|
     if @review.save
       format.html { redirect_to boat_path(@boat) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_05_071843) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_05_092240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +74,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_071843) do
     t.bigint "boat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["boat_id"], name: "index_reviews_on_boat_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -97,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_071843) do
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "boats"
+  add_foreign_key "reviews", "users"
 end
